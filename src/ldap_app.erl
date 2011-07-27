26ba305374d01e2ddf6d932805c3955a35d1b142
Taylor Venable <taylor@metasyntax.net>
2011-07-27 13:39:38 -0400

    Add skeleton files from rebar.

---
+++ src/ldap_app.erl
@@ -0,0 +1,16 @@
+-module(ldap_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    ldap_sup:start_link().
+
+stop(_State) ->
+    ok.
